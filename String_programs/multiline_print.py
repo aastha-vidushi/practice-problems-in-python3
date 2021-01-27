44eb49cfbebb0a8ad3f6fefd39b3ae66dda8a360
Aastha Vidushi <aastha.vidushi@sungardas.com>
2021-01-28 01:34:55 +0530

    Added a new folder that contains multiline print statement with emoji using Unicode.

---
+++ String_programs/multiline_print.py
@@ -0,0 +1,14 @@
+'''
+This is a simple python file to print multi-line statements that ends with a smiley using Unicode.
+'''
+
+
+statement = '''
+Hello there!
+I am Aastha Vidushi, a pythonist.
+I like to interact with people to share knowledge and have some great conversations.
+You can sometimes find me sharing memes, eating pizza and exploring different places.
+Happy to see you here. Have a great day.\U0001F60A
+'''
+
+print(statement)
